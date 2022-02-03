6752448b08cfa60bc78c06d7235d79896a41ba6a
Brody Sears <brodysears@Brodys-Mac-mini.local>
2022-02-03 16:32:57 -0700

    completed networking code to parse and create the object

--- Pokedex/Pokedex/Controllers/Networking/NetworkController.swift
+++ Pokedex/Pokedex/Controllers/Networking/NetworkController.swift
@@ -14,13 +14,36 @@ class NetworkController {
     //static private let urlPokeComponent = "/pokemon"
     
     // MARK: - Fetch Func
-    static func fetchPokemon(name searchTerm: String, completion: @escaping ([Pokemon]?) -> Void ) {
+    static func fetchPokemon(name searchTerm: String, completion: @escaping (Pokemon?) -> Void ) {
+        
         guard let baseURL = URL(string: baseURLString) else { return }
         var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
         urlComponents?.path = "/pokemon/\(searchTerm.lowercased())"
         
         guard let finalURL = urlComponents?.url else { return }
         print(finalURL)
+        
+        //Data Task
+        URLSession.shared.dataTask(with: finalURL) { taskData, _, error in
+            if let error = error {
+                print("There was an error fetching the data. The url is \(finalURL), the error is \(error.localizedDescription)")
+                completion(nil)
+            }
+            guard let pokemonData = taskData else { return }
+            
+            do {
+                //try
+                if let topLevelDictionary = try JSONSerialization.jsonObject(with: pokemonData, options: .fragmentsAllowed) as? [String: Any] {
+            
+                    let pokemon = Pokemon(dictionary: topLevelDictionary)
+                    completion(pokemon)
+                }
+            } catch {
+                print("Encountered error when decoding the data:", error.localizedDescription)
+                completion(nil)
+            }
+                    
+        }.resume()
     }
     
 }// end of class
